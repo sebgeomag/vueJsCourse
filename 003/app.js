8f13d07412954b1b14957d00a4056028b7a8b2ea
SebGeoMag <bastl2301@hotmail.com>
2026-07-26 06:29:31 +0200

    Event Binding

--- 003/app.js
+++ 003/app.js
@@ -3,11 +3,10 @@ const app = Vue.createApp({
         return {
             myName: "Sebastian",
             myAge: 44,
-            myAgePlusFive: myAge+5,
             randomNumber: Math.random(),
             courseGoalA: "<h3>Blubb</h3>",
             courseGoalB: "Become Vue Master",
-            vueLink: "https://www.google.de",
+            vueLink: "https://media.istockphoto.com/id/816752606/de/foto/tv-testkarte-oder-testmuster-generisch.jpg?s=1024x1024&w=is&k=20&c=_t7toofVhbQ5K_l7TxyPGh7GtUDVuhCmvtn1G5K4E2k=",
         }
     },
     methods: {
