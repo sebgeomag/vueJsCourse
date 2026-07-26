d243266485e0e8dcf68f6ee3e3a3f56a424e30bd
SebGeoMag <bastl2301@hotmail.com>
2026-07-26 07:30:50 +0200

    Added Event Section Assignment

--- 004/app.js
+++ 004/app.js
@@ -2,8 +2,24 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      myName: ""
     };
   },
+  methods: {
+    increment(){
+      this.counter++;
+    },
+    decrement(){
+      this.counter--;
+    },
+    setName(event, lastname){
+      this.myName = event.target.value + ' ' + lastname;
+    },
+    clickTheButton(){
+      //event.preventDefault();
+      alert('Clicked the button...');
+    }
+  }
 });
 
 app.mount('#events');
