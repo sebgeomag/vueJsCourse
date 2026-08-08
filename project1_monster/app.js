c73fc3a28a5fa3ffbe7b1824ae7989c09ea0af8b
SebGeoMag <bastl2301@hotmail.com>
2026-08-08 19:50:00 +0200

    Worked on Monster Game

--- project1_monster/app.js
+++ project1_monster/app.js
@@ -8,12 +8,35 @@ const app = Vue.createApp({
             playerHealth: 100,
             monsterHealth: 100,
             attackCounter: 0,
-            specialAttackDisabled: true
+            specialAttackDisabled: true,
+            winner: null
         };
     },
     computed:{
         specialAttackVisible() {
         return this.specialAttackDisabled
+        }
+    },
+    watch:{
+        playerHealth(value){
+            if(value<=0 && this.monsterHealth<=0){
+                //Draw
+                this.winner = 'draw';
+                this.resetGame();
+            }else if(value<=0){
+                //Player lost
+                this.winner = 'monster'
+                this.resetGame();
+            }
+        },
+        monsterHealth(value){
+            if(value<=0 && this.playerHealth<=0){
+                this.winner = 'draw';
+                this.resetGame();
+            }else if(value<=0){
+                this.winner = 'player';
+                this.resetGame();
+            }
         }
     },
     methods:{
@@ -39,7 +62,7 @@ const app = Vue.createApp({
             this.specialAttackDisabled = true;
         },
         healPlayer(){
-            this.attackCounter++;
+            //this.attackCounter++;
             const healValue = getRandomValue(8,20);
             if(this.playerHealth + healValue > 100){
                 this.playerHealth = 100;
@@ -47,11 +70,16 @@ const app = Vue.createApp({
                 this.playerHealth += healValue;
             }
             this.attackPlayer();
-                        this.attackCounter++;
+            this.attackCounter++;
             if(this.attackCounter===3){
                 this.attackCounter=0;
                 this.specialAttackDisabled = false;
             }
+        },
+        resetGame(){
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.attackCounter = 0;
         }
     }
 });
