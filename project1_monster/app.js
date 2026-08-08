283d2069438c73e99f616a3a266c7591f174554c
SebGeoMag <bastl2301@hotmail.com>
2026-08-08 10:06:38 +0200

    Worked on Monster Game

--- project1_monster/app.js
+++ project1_monster/app.js
@@ -1,23 +1,57 @@
 function getRandomValue(max, min){
-    return Math.floor(Math.random() * (mmx-min))+min;
+    return Math.floor(Math.random() * (max-min))+min;
 }
 
 const app = Vue.createApp({
     data() {
         return { 
             playerHealth: 100,
-            monsterHealth: 100
+            monsterHealth: 100,
+            attackCounter: 0,
+            specialAttackDisabled: true
         };
+    },
+    computed:{
+        specialAttackVisible() {
+        return this.specialAttackDisabled
+        }
     },
     methods:{
         attackMonster(){
+            console.log(this.attackCounter);
             const attackValue = getRandomValue(12,5);
             this.monsterHealth -= attackValue;
             this.attackPlayer();
+            this.attackCounter++;
+            if(this.attackCounter===3){
+                this.attackCounter=0;
+                this.specialAttackDisabled = false;
+            }
         },
         attackPlayer(){
             const attackValue = getRandomValue(15,8);
             this.playerHealth -= attackValue;
+        },
+        specialAttackMonster(){
+            const attackValue = getRandomValue(10,25);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
+            this.specialAttackDisabled = true;
+        },
+        healPlayer(){
+            this.attackCounter++;
+            const healValue = getRandomValue(8,20);
+            if(this.playerHealth + healValue > 100){
+                this.playerHealth = 100;
+            }else{
+                this.playerHealth += healValue;
+            }
+            this.attackPlayer();
+                        this.attackCounter++;
+            if(this.attackCounter===3){
+                this.attackCounter=0;
+                this.specialAttackDisabled = false;
+            }
         }
     }
 });
